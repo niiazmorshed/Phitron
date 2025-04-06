512649786339f3214bf128ad12329a4bf5f347db
Niaz Morshed <niazmorshedrafi@gmail.com>
2025-04-06 06:33:15 +0600

    Added the extra question for module-1

---
+++ Introduction to Programming/Module-1/index-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int num1 , num2 ;
+    printf("Enter The First Number: ");
+    scanf("%d", &num1); 
+    printf("Enter The Second Number: ");
+    scanf("%d", &num2); 
+    printf("Second Number: %d \nFirst Number: %d",num2, num1);
+    return 0;
+}

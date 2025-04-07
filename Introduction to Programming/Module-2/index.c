61e7e78f62dbfeda0084973c1191513c1cb972ec
Niaz Morshed <niazmorshedrafi@gmail.com>
2025-04-07 15:44:51 +0600

    Completed Module 2

--- Introduction to Programming/Module-2/index.c
+++ Introduction to Programming/Module-2/index.c
@@ -1,7 +1,39 @@
-#include<stdio.h>
+// Extra Practice Problem
+//  Task 1
 
-int main(){
-    
-    printf("Joy Bangla!");
-    return 0;
+// #include <stdio.h>
+// int main()
+// {
+//     int num;
+//     printf("Please Enter Your Number: ");
+//     scanf("%d", &num);
+//     if (num % 2 == 0)
+//     {
+//         printf("%d is an Even Number", num);
+//     }
+//     else
+//     {
+//         printf("%d is an Odd Number", num);
+//     }
+//     return 0;
+// }
+
+
+// Task-2
+
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Please Enter Your Number: ");
+    scanf("%d", &num);
+    if (num > 0){
+        printf("%d is a Positive Number", num);
+    }
+    else if(num == 0){
+        printf("%d is a Non Negative integer Value", num);
+    }
+    else {
+        printf("%d is a Negative Number", num);
+    }
 }

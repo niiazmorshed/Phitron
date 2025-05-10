f64cdfd2bd8d59280f427bb68d93233213871767
Niaz Morshed <niazmorshedrafi@gmail.com>
2025-05-10 11:37:22 +0600

    Done with function and pointer including assignment 3

--- Introduction to Programming/Function/test.c
+++ Introduction to Programming/Function/test.c
@@ -1,98 +1,41 @@
+// Practice problem 4
+// #include <stdio.h>
+
+// int change_it(int a[], int n){
+//     a[n-1] = 100;
+// }
+
+// int main()
+// {
+//   int n;
+//   scanf("%d", &n);
+//   int a[n];
+//   for (int i=0; i<n; i++){
+//     scanf("%d", &a[i]);
+//   }
+//   change_it(a,n);
+//   for (int i=0; i<n; i++){
+//     printf("%d ", a[i]);
+//   }
+//   return 0;
+// }
+
+// Practice problem 5
 #include <stdio.h>
+int swap_it(int *n, int *m)
+{
+  int temp = *n;
+  *n = *m;
+  *m = temp;
+}
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    int hash1 = 1;
-    int space1 = n;
-    int test1, test2;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int k = 1; k < space1; k++)
-        {
-            printf(" ");
-        }
-        if (i % 2 != 0)
-        {
-            for (int j = 1; j <= hash1; j++)
-            {
-                printf("#");
-                test1 = j;
-            }
-        }
-        else
-        {
-            for (int j = 1; j <= hash1; j++)
-            {
-                printf("-");
-                test2 = j;
-            }
-        }
-        printf("\n");
-        hash1 += 2;
-        space1--;
-    }
-    int temp = 0;
-    if (test1 > test2)
-    {
-        temp = test2;
-    }
-    else
-    {
-        temp = test1;
-    }
-    int hash2 = temp;
-    int space2 = 1;
-    for (int i = 1; i <= n - 1; i++)
-    {
-        if (n % 2 != 0)
-        {
-            for (int k = 0; k < space2; k++)
-            {
-                printf(" ");
-            }
-            if (i % 2 == 0)
-            {
-                for (int j = 1; j <= temp; j++)
-                {
-                    printf("#");
-                }
-            }
-            else
-            {
-                for (int j = 1; j <= temp; j++)
-                {
-                    printf("-");
-                }
-            }
-            printf("\n");
-            temp -= 2;
-            space2++;
-        }
-        else
-        {
-            for (int k = 0; k < space2; k++)
-            {
-                printf(" ");
-            }
-            if (i % 2 == 0)
-            {
-                for (int j = 1; j <= temp; j++)
-                {
-                    printf("-");
-                }
-            }
-            else
-            {
-                for (int j = 1; j <= temp; j++)
-                {
-                    printf("#");
-                }
-            }
-            printf("\n");
-            temp -= 2;
-            space2++;
-        }
-    }
-    return 0;
+  int n, m;
+  scanf("%d %d", &n, &m);
+  int *ptr1, *ptr2;
+  ptr1 = &n;
+  ptr2 = &m;
+  swap_it(ptr1, ptr2);
+  printf("%d %d", n, m);
+  return 0;
 }

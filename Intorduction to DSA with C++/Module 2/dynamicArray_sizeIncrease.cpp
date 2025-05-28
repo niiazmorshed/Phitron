41e348d3d42b6b5002708fe2cc7f1e8d2273cc72
Niaz Morshed <niazmorshedrafi@gmail.com>
2025-05-29 00:21:49 +0600

    Add Module 2

---
+++ Intorduction to DSA with C++/Module 2/dynamicArray_sizeIncrease.cpp
@@ -0,0 +1,30 @@
+// Process ager mtoi ekhane
+// difference hocceh dynamic arry te
+// array ta delete kora jay
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int *a = new int[3];
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> a[i];
+    }
+    int *b = new int[5];
+    for (int i = 0; i < 3; i++)
+    {
+        b[i] = a[i];
+    }
+    b[3] = 40;
+    b[4] = 50;
+    delete[]a;
+    // now print
+    for (int i = 0; i < 5; i++)
+    {
+        cout << b[i] << "\n";
+    }
+    return 0;
+}

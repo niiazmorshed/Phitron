bd87536371c1eebd983248dc728ba3ff0e6def93
Niaz Morshed <121424001+niiazmorshed@users.noreply.github.com>
2025-07-11 00:37:52 +0600

    solved practice 1 & 2 from week-3

--- Data Structure/Week 3 Practice day 1 (Doubly Linked List)/index1.cpp
+++ Data Structure/Week 3 Practice day 1 (Doubly Linked List)/index1.cpp
@@ -28,7 +28,34 @@ void insert_at_tail(Node *&head, Node *&tail, int val)
     newNode->prev = tail;
     tail = newNode;
 }
+int len(Node *head)
+{
+    Node *temp = head;
+    int count = 0;
+    while (temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
 
+bool same_or_not(Node *&head1, Node *&head2)
+{
+    bool flag = true;
+    Node *temp1 = head1;
+    Node *temp2 = head2;
+    while (temp1 != NULL)
+    {
+        if (temp1->val != temp2->val)
+        {
+            flag = false;
+        }
+        temp1 = temp1->next;
+        temp2 = temp2->next;
+    }
+    return flag;
+}
 void print_forward(Node *head)
 {
     Node *temp = head;
@@ -42,8 +69,10 @@ void print_forward(Node *head)
 
 int main()
 {
-    Node *head = NULL;
-    Node *tail = NULL;
+    Node *head1 = NULL;
+    Node *tail1 = NULL;
+    Node *head2 = NULL;
+    Node *tail2 = NULL;
     int val;
     while (true)
     {
@@ -52,8 +81,28 @@ int main()
         {
             break;
         }
-        insert_at_tail(head, tail, val);
+        insert_at_tail(head1, tail1, val);
     }
-    print_forward(head);
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head2, tail2, val);
+    }
+    int lenA = len(head1);
+    int lenB = len(head2);
+    if (lenA != lenB)
+    {
+        cout << "NO";
+    }
+    else
+    {
+        bool flag = same_or_not(head1, head2);
+        flag ? cout << "YES" : cout << "NO";
+    }
+
     return 0;
 }
